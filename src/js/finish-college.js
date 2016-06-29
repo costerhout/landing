5eb0d82810d3b86dbc591b07aafbb90f7c3224e4
Colin Osterhout <colixx@gmail.com>
2016-06-29 13:19:22 -0800

    First draft version.

--- src/js/finish-college.js
+++ src/js/finish-college.js
@@ -1,40 +1,52 @@
-;(function(mymodule) {
+/**
+* @Author: Colin Osterhout <ctosterhout>
+* @Date:   2016-06-13T08:44:26-08:00
+* @Email:  ctosterhout@alaska.edu
+* @Project: BERT
+* @Last modified by:   ctosterhout
+* @Last modified time: 2016-06-29T12:51:47-08:00
+* @License: Released under MIT License. Copyright 2016 University of Alaska Southeast.  For more details, see https://opensource.org/licenses/MIT
+*/
+
+(function() {
     'use strict';
 
-    mymodule(window.jQuery);
-}(function($) {
-    'use strict';
     // Wait for jQuery to be fully loaded
     require(['jquery'], function($){
         // Wait for the DOM to be fully loaded
         $(function () {
-            // Save away the title and subtitles query for later use
-            var $titles = $('.title-change'),
-                $subtitles = $('.fadein-end'),
-                timeFade = 600,
-                timeHold = 2000,
-                timeRotate = 3000,
-                // Set the initial state
-                i = -1,
-                rotateTitle = function () {
-                    // If we're not at the last title, then fade in the title, delay, and then fade out
-                    i++;
+            $('.goto-request-form').click(function() {
+                // Get the target to scroll to
+                var $target = $('#request-form'), timeAnimate = 500;
 
-                    // Set the title to the desired index
-                    if (i < $titles.length - 1) {
-                        $titles.eq(i)
-                            .fadeIn(timeFade)
-                            .delay(timeHold)
-                            .fadeOut(timeFade, rotateTitle);
-                    } else if (i === $titles.length - 1) {
-                        // We're at the end. Fade in the last title and fade in the subtitle
-                        $titles.eq(i).fadeIn(timeFade);
-                        $subtitles.fadeIn(timeFade);
-                    }
-                };
+                // Begin the animation
+                $('html, body').animate({
+                    scrollTop: $target.offset().top
+                }, timeAnimate);
 
-            // Begin rotation
-            rotateTitle();
+                // Put the cursor in the first field
+                $('#entry_1738965336').focus();
+
+                return false;
+            });
+
+            $('.zopim-fade-in').hide();
+
+            // Determine if the Zopim client is loaded up and available
+            if (typeof $zopim !== 'undefined') {
+                // Tell the zopim client to hide immediately upon API ready
+                $zopim(function () {
+                    $zopim.livechat.window.hide();
+
+                    // Add a click handler to open up the zopim livechat window
+                    $('.chat-open').click(function () {
+                        $zopim.livechat.window.show();
+                    });
+
+                    // Fade the open chat button in
+                    $('.zopim-fade-in').fadeIn(500);
+                });
+            }
         });
     });
-}));
+})();
